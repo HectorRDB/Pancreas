72178cd21024a40ab1b382e0a37309cba1833a86
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-01 23:22:08 -0700

    Update 3-sc3.sh

--- Scripts/singleDataset/3-sc3.sh
+++ Scripts/singleDataset/3-sc3.sh
@@ -8,14 +8,14 @@
 echo "baron"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_filt.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/baron_SC3.csv"
-Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out -p plot > 3-baron.out 2>&1
+Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out > 3-baron.out 2>&1
 
 # echo "muraro"
 # loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_filt.rds"
 # out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/muraro_SC3.csv"
-# Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out -p plot > 3-muraro.out 2>&1
+# Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out > 3-muraro.out 2>&1
 
 echo "segerstolpe"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_filt.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/segerstolpe_SC3.csv"
-Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out -p plot > 3-segerstolpe.out 2>&1
+Rscript --verbose  3-sc3.R -n 32 -l $loc -o $out > 3-segerstolpe.out 2>&1
