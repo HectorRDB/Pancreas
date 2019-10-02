16abb7720c5cb9343d09cc09b795af30c20519ea
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-01 23:06:52 -0700

    Update 2-zinb.sh

--- Scripts/singleDataset/2-zinb.sh
+++ Scripts/singleDataset/2-zinb.sh
@@ -2,13 +2,29 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=32
 #SBATCH --nodes=1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_filt.rds"
-out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_zinbWs.rds"
-Rscript --verbose  3-zinb.R -n 20 -l $loc -o $out > 3b.out 2>&1
+echo "baron"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_filt.rds"
+out="/scratch/users/singlecell/Pancreas/ProcessedData/baron_zinbW.rds"
+plot="/accounts/projects/epurdom/singlecell/Pancreas/Figures/EDA/baron"
+Rscript --verbose  2-zinb.R -n 32 -l $loc -o $out -p plot > 2-baron.out 2>&1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_filt.rds"
-out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_zinbWs.rds"
-Rscript --verbose  3-zinb.R -n 20 -l $loc -o $out > 3a.out 2>&1
+echo "muraro"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_filt.rds"
+out="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_zinbW.rds"
+plot="/accounts/projects/epurdom/singlecell/Pancreas/Figures/EDA/muraro"
+Rscript --verbose  2-zinb.R -n 32 -l $loc -o $out -p plot > 2-muraro.out 2>&1
+
+echo "segerstolpe"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_filt.rds"
+out="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_zinbW.rds"
+plot="/accounts/projects/epurdom/singlecell/Pancreas/Figures/EDA/segerstolpe"
+Rscript --verbose  2-zinb.R -n 32 -l $loc -o $out -p plot > 2-segerstolpe.out 2>&1
+
+echo "xin"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/xin_filt.rds"
+out="/scratch/users/singlecell/Pancreas/ProcessedData/xin_zinbW.rds"
+plot="/accounts/projects/epurdom/singlecell/Pancreas/Figures/EDA/xin"
+Rscript --verbose  2-zinb.R -n 32 -l $loc -o $out -p plot > 2-xin.out 2>&1
