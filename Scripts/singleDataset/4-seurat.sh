4880906c41cee9210a90d5477d6bb9a6d08780e4
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-01 23:23:28 -0700

    Update 4-seurat.sh

--- Scripts/singleDataset/4-seurat.sh
+++ Scripts/singleDataset/4-seurat.sh
@@ -5,10 +5,17 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_filt.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMethod/SMARTer_nuclei_MOp_Seurat.csv"
-Rscript --verbose  5-seurat.R -l $loc -o $out > 5b.out 2>&1
+echo "baron"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_filt.rds"
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/baron_Seurat.csv"
+Rscript --verbose  4-seurat.R -l $loc -o $out > 4-baron.out 2>&1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_filt.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMethod/SMARTer_cells_MOp_Seurat.csv"
-Rscript --verbose  5-seurat.R -l $loc -o $out > 5a.out 2>&1
+# echo "muraro"
+# loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_filt.rds"
+# out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/muraro_Seurat.csv"
+# Rscript --verbose  4-seurat.R -l $loc -o $out > 4-muraro.out 2>&1
+
+echo "segerstolpe"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_filt.rds"
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/segerstolpe_Seurat.csv"
+Rscript --verbose  4-seurat.R -l $loc -o $out > 4-segerstolpe.out 2>&1
