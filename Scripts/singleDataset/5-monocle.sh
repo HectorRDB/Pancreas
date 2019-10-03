d2e884a95822fa8e0e551176e42ec717a6cbc54e
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-02 23:40:36 -0700

    Update 5-monocle.sh

--- Scripts/singleDataset/5-monocle.sh
+++ Scripts/singleDataset/5-monocle.sh
@@ -5,10 +5,17 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_zinbWs.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMethod/SMARTer_nuclei_MOp_Monocle.csv"
-Rscript --verbose  6-monocle.R -l $loc -o $out > 6b.out 2>&1
+echo "baron"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_zinbW.rds"
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/baron_Monocle.csv"
+Rscript --verbose  5-monocle.R -l $loc -o $out > 5-baron.out 2>&1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_zinbWs.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMethod/SMARTer_cells_MOp_Monocle.csv"
-Rscript --verbose  6-monocle.R -l $loc -o $out > 6a.out 2>&1
+# echo "muraro"
+# loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_zinbW.rds"
+# out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/muraro_Monocle.csv"
+# Rscript --verbose  5-monocle.R -l $loc -o $out > 5-muraro.out 2>&1
+
+echo "segerstolpe"
+loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_zinbW.rds"
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/segerstolpe_Monocle.csv"
+Rscript --verbose  5-monocle.R -l $loc -o $out > 5-segerstolpe.out 2>&1
