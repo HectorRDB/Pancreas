63242272602fb28d6cffb24fbe7223445d0ef794
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-03 15:57:37 -0700

    Rsec and Dune comp

--- Scripts/Dune-Comparison/Comp1.sh
+++ Scripts/Dune-Comparison/Comp1.sh
@@ -8,11 +8,11 @@
 echo "baron"
 loc="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/baron"
 rsec="/scratch/users/singlecell/Pancreas/ProcessedData/baron_Rsec.rds"
-out="/accounts/projects/epurdom/singlecell/Pancreas/Data/Dune/baron_comp3"
-Rscript --verbose  Dune.R -n 32 -l $loc -o $out -S "1.5.50" -C "10" -m "k_25" -r $rsec > baron3.out 2>&1
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/Dune/baron_comp1"
+Rscript --verbose  Dune.R -n 32 -l $loc -o $out -S "1.2.50" -C "15" -m "k_40" -r $rsec > baron1.out 2>&1
 
 echo "segerstolpe"
 loc="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/segerstolpe"
 rsec="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_Rsec.rds"
-out="/accounts/projects/epurdom/singlecell/Pancreas/Data/Dune/segerstolpe_comp4"
-Rscript --verbose  Dune.R -n 32 -l $loc -o $out -S "1.5.50" -C "10" -m "k_25" -r $rsec > segerstolpe3.out 2>&1
+out="/accounts/projects/epurdom/singlecell/Pancreas/Data/Dune/segerstolpe_comp1"
+Rscript --verbose  Dune.R -n 32 -l $loc -o $out -S "1.2.50" -C "0" -m "k_40" -r $rsec > segerstolpe1.out 2>&1
