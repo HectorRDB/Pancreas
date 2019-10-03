d45d56903971ee9050a797007a6e801984b6720e
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-03 15:59:10 -0700

    Update 6-RSEC.sh

--- Scripts/singleDataset/6-RSEC.sh
+++ Scripts/singleDataset/6-RSEC.sh
@@ -9,7 +9,7 @@ echo "baron"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_zinbW.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/baron_Rsec.csv"
 sce="/scratch/users/singlecell/Pancreas/ProcessedData/baron_Rsec.rds"
-Rscript --verbose 6-Rsec.R -l $loc -o $out -s $sce > 6-baron.out 2>&1
+Rscript --verbose 6-RSEC.R -l $loc -o $out -s $sce > 6-baron.out 2>&1
 
 # echo "muraro"
 # loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_zinbW.rds"
@@ -20,4 +20,4 @@ echo "segerstolpe"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_zinbW.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/singleMethod/segerstolpe_Rsec.csv"
 sce="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_Rsec.rds"
-Rscript --verbose 6-Rsec.R -l $loc -o $out -s $sce > 6-segerstolpe.out 2>&1
+Rscript --verbose 6-RSEC.R -l $loc -o $out -s $sce > 6-segerstolpe.out 2>&1
