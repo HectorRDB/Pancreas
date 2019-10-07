ad98d900f27ef1ac994aca8909aa1986ccb86113
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-07 10:33:30 -0700

    Update 01-zinb_tsne.sh

--- Scripts/Viz/01-zinb_tsne.sh
+++ Scripts/Viz/01-zinb_tsne.sh
@@ -8,14 +8,14 @@
 echo "baron"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/baron_zinbW.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/tSNE/baron.csv"
-  Rscript --verbose  01-zinb_tsne.R -l $loc -o $out -K 1 > 5-baron.out 2>&1
+  Rscript --verbose  01-zinb_tsne.R -l $loc -o $out -K 1 > 1-baron.out 2>&1
 
 # echo "muraro"
 # loc="/scratch/users/singlecell/Pancreas/ProcessedData/muraro_zinbW.rds"
 # out="/accounts/projects/epurdom/singlecell/Pancreas/Data/tSNE/muraro.csv"
-# Rscript --verbose  5-monocle.R -l $loc -o $out -k 3 > 5-muraro.out 2>&1
+# Rscript --verbose  01-zinb_tsne.R -l $loc -o $out -k 3 > 1-muraro.out 2>&1
 
 echo "segerstolpe"
 loc="/scratch/users/singlecell/Pancreas/ProcessedData/segerstolpe_zinbW.rds"
 out="/accounts/projects/epurdom/singlecell/Pancreas/Data/tSNE/segerstolpecsv"
-  Rscript --verbose  01-zinb_tsne.R -l $loc -o $out -K 2 > 5-segerstolpe.out 2>&1
+  Rscript --verbose  01-zinb_tsne.R -l $loc -o $out -K 2 > 2-segerstolpe.out 2>&1
