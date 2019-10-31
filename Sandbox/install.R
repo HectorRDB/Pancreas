3e704ca90f808fd9895f6e61ef4f6e7e1923a465
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-31 16:24:36 -0700

    Update install.R

--- Sandbox/install.R
+++ Sandbox/install.R
@@ -1,3 +1,5 @@
 library(BiocManager)
-install(version = "3.10", update = TRUE, ask = FALSE)
-install.packages("~/Dune/", repos = NULL, )
+install(version = "3.10", update = TRUE, ask = FALSE,
+        lib = "/accounts/projects/epurdom/singlecell/R/x86_64-pc-linux-gnu-library/3.6")
+install.packages("~/Dune/", repos = NULL,
+                 lib = "/accounts/projects/epurdom/singlecell/R/x86_64-pc-linux-gnu-library/3.6")
